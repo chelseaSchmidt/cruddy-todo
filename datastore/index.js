2d118f4f9d3c28e6e96279f4a298239add91f650
Chelsea Schmidt <cschmidt331@gmail.com>
2020-06-02 09:19:24 -0700

    Refactored delete operation and readAll test

--- datastore/index.js
+++ datastore/index.js
@@ -63,14 +63,14 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  fs.unlink(makeFilePath(id), (err) => {
+    if (err) {
+      // report an error if item not found
+      callback(err);
+    } else {
+      callback();
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
